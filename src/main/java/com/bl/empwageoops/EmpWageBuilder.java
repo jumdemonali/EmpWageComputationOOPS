aa7784c27ff85fb64305194235ee35899897d249
jumdemonali <jumdemonali@gmail.com>
2021-09-01 19:33:40 +0530

    Added: Switch case

--- src/main/java/com/bl/empwageoops/EmpWageBuilder.java
+++ src/main/java/com/bl/empwageoops/EmpWageBuilder.java
@@ -2,23 +2,20 @@ package com.bl.empwageoops;
 
 public class EmpWageBuilder {
     public static final int RATE_PER_HR = 20;
-    int IS_FULL_TIME = 1;
-    int IS_PART_TIME = 2;
+    static final int IS_FULL_TIME = 1;
+    static  final int IS_PART_TIME = 2;
     public void employeeWage() {
         System.out.println("Welcome to Employee Wage Computation Program!!");
         int empHrs = 0, totalEmpHrs = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present full time");
-            totalEmpHrs = 8;
-        } else if (empCheck == IS_PART_TIME)
-        {
-            System.out.println("Employee is Present part time");
-            totalEmpHrs = 4;
-        }
-        else {
-            System.out.println("Employee is Absent");
-            totalEmpHrs = 0;
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+            default:
+                empHrs = 0;
         }
         totalEmpHrs += empHrs;
         int totalEmpWage = totalEmpHrs * RATE_PER_HR;
