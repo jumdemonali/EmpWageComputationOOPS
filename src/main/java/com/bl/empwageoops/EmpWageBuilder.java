ce0b889c25ee2ed64eb52788b6c31f0f888fb6f2
jumdemonali <jumdemonali@gmail.com>
2021-09-01 18:16:18 +0530

    Added: Welcome message

---
+++ src/main/java/com/bl/empwageoops/EmpWageBuilder.java
@@ -0,0 +1,11 @@
+package com.bl.empwageoops;
+
+public class EmpWageBuilder {
+    public void employeeWage() {
+        System.out.println("Welcome to Employee Wage Computation Program!!");
+    }
+    public static void main(String[] args) {
+        EmpWageBuilder emp = new EmpWageBuilder();
+        emp.employeeWage();
+    }
+}
