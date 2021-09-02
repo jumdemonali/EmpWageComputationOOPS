da70a45ad44711fc33e1be1621d24e12c9436896
jumdemonali <jumdemonali@gmail.com>
2021-09-02 15:12:26 +0530

    Added: Checking of wage for 100hr 0r 20 days

--- src/main/java/com/bl/empwageoops/EmpWageBuilder.java
+++ src/main/java/com/bl/empwageoops/EmpWageBuilder.java
@@ -1,38 +1,48 @@
 package com.bl.empwageoops;
 
 public class EmpWageBuilder {
-    int wagePerHrs=20;
-    int dailyWage;
-    int workHrs=0;
-    int totalempWage =0;
-    public static void main(String[] args) {
-        for(int day=1;day<=20;day++) {
-            int randomCheck = (int) (Math.floor(Math.random()*10)%3);
-            EmpWageBuilder obj=new EmpWageBuilder();
-            obj.wageCal(randomCheck);
+
+        static final int WAGE_PER_HR = 20;
+        static final int FULL_DAY_HR = 8;
+        static final int PART_TIME_HR = 4;
+        static final int MAX_WORKING_DAYS = 20;
+        static final int MAX_HRS_PER_MONTH = 100;
+        static final int IS_FULL_TIME = 0;
+        static final int IS_PART_TIME = 1;
+
+        public void employeeWage() {
+            System.out.println("Welcome to Employee Wage Builder Program!!");
+            int totWage = 0;
+            int totalWorkingHrs = 0;
+            int totalWorkingDays = 0;
+            int empHrs = 0;
+
+            while (totalWorkingHrs < MAX_HRS_PER_MONTH && totalWorkingDays < MAX_WORKING_DAYS) {
+                totalWorkingDays++;
+                double empCheck = (Math.floor(Math.random() * 10) % 3);
+                switch ((int) empCheck) {
+                    case IS_FULL_TIME:
+                        System.out.println("Employee is present full time");
+                        empHrs = FULL_DAY_HR;
+                        break;
+                    case IS_PART_TIME:
+                        System.out.println("Employee is present part time");
+                        empHrs = PART_TIME_HR;
+                        break;
+                    default:
+                        System.out.println("Employee is absent");
+                        empHrs = 0;
+                }
+                totalWorkingHrs += empHrs;
+            }
+            totWage = totalWorkingHrs * WAGE_PER_HR;
+            System.out.println("Total working hours: " +totalWorkingHrs);
+            System.out.println("Total working Days: " +totalWorkingDays);
+            System.out.println("Total Wage is " +totWage);
+        }
+
+        public static void main(String[] args) {
+            EmpWageBuilder emp = new EmpWageBuilder();
+            emp.employeeWage();
         }
     }
-
-    public void wageCal(int randomCheck) {
-        int dailyWage = 0;
-
-        switch (randomCheck) {
-            case 1: {
-                workHrs = 4;
-                break;
-            }
-            case 2: {
-                workHrs = 8;
-                break;
-            }
-            default: {
-
-                workHrs = 0;
-            }
-        }
-            dailyWage = wagePerHrs * workHrs;
-            System.out.println("DailySalary:" + dailyWage);
-
-        }
-
-}
