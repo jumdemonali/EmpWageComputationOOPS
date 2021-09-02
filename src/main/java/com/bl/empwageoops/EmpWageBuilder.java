2e3e6775ed735ff09b71908f34890358fa92dc17
jumdemonali <jumdemonali@gmail.com>
2021-09-02 12:01:43 +0530

    Added: Switch case statement

--- src/main/java/com/bl/empwageoops/EmpWageBuilder.java
+++ src/main/java/com/bl/empwageoops/EmpWageBuilder.java
@@ -3,27 +3,33 @@ package com.bl.empwageoops;
 public class EmpWageBuilder {
     public static final int RATE_PER_HR = 20;
     static final int IS_FULL_TIME = 1;
-    static  final int IS_PART_TIME = 2;
-    public void employeeWage() {
-        System.out.println("Welcome to Employee Wage Computation Program!!");
-        int empHrs = 0, totalEmpHrs = 0;
-        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-            default:
-                empHrs = 0;
-        }
-        totalEmpHrs += empHrs;
-        int totalEmpWage = totalEmpHrs * RATE_PER_HR;
-        System.out.println("Total emp wage is: " + totalEmpWage);
-    }
+    static final int IS_PART_TIME = 2;
 
     public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program!!");
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
         EmpWageBuilder emp = new EmpWageBuilder();
-        emp.employeeWage();
+        emp.employeeWage(empCheck);
     }
-}
+
+    public void employeeWage(int empCheck) {
+        int empHrs = 0, totalEmpHrs = 0, totalEmpWage = 0, empWage = 0;
+        switch (empCheck) {
+            case IS_FULL_TIME: {
+                empHrs = 8;
+                empWage = empHrs * RATE_PER_HR;
+                break;
+            }
+            case IS_PART_TIME: {
+                empHrs = 4;
+                empWage = empHrs * RATE_PER_HR;
+                break;
+            }
+            default: {
+                empHrs = 0;
+            }
+        }
+    }
+    }
+
+
